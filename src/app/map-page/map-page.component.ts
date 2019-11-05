30c88190d6213ddf2ba82d2251eddd4d35e3f53b
Aleksandr Korenev <korenev6052@gmail.com>
2019-11-06 02:35:37 +0300

    NCU-6 Создал основные компоненты, настроил роуты

---
+++ src/app/map-page/map-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'po-map-page',
+  templateUrl: './map-page.component.html',
+  styleUrls: ['./map-page.component.css']
+})
+export class MapPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
